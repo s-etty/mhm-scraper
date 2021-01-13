ca4520312c44629530c0e5e2bf90cb0b48af7fe1
Jackie Kinsler <jackiekinsler@gmail.com>
2021-01-12 19:39:19 -0800

    Created log.txt to store results from scrape.

--- mhm-scraper.py
+++ mhm-scraper.py
@@ -1,6 +1,14 @@
+# pip3 freeze > requirements.txt
+# pip3 install -r requirements.txt
+
 import requests
 from bs4 import BeautifulSoup
-from datetime import date, timedelta
+from datetime import datetime
+from pathlib import Path
+
+# I think we can run this as a cron job... and it'll just automatically add to the log
+
+now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 #site URL
 url = "https://www.skihood.com/en/the-mountain/conditions"
@@ -11,4 +19,48 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 #extract the parking lot table using class
-lot_status = soup.find_all('div', class_='conditions-info parking-lots')
+lot_table = soup.find_all('div', class_='conditions-info parking-lots')
+
+# extract parking status of each individual lot
+parking_table = lot_table[0].find_all('td', class_='status-status')
+
+# create an array of parking statuses [main, sunrise, hrm, twilight]
+status_array = []
+for element in parking_table:
+    status = str(element.string)
+    status_array.append(status)
+
+# this is clunky, but whatever... it's all clunky.
+main = {"name" : "Main",
+        "time" : now, 
+        "status" : status_array[0]
+        }
+sunrise = {"name" : "Sunrise",
+        "time" : now, 
+        "status" : status_array[1]
+        }
+hrm = {"name" : "Hrm",
+        "time" : now, 
+        "status" : status_array[2]
+        }
+twilight = {"name" : "Twilight",
+        "time" : now, 
+        "status" : status_array[3]
+        }
+
+# check to see if log.txt exists or not 
+# if it exists, print the time, lot name, and lot status 
+if Path("log.txt").is_file():
+    print(f"Adding scraped data from {now}")
+    file = open("log.txt", "a")
+    file.write(f"{now} | {main['name'].ljust(8)} | {main['status']}\n")
+    file.write(f"{now} | {sunrise['name'].ljust(8)} | {sunrise['status']}\n")
+    file.write(f"{now} | {hrm['name'].ljust(8)} | {hrm['status']}\n")
+    file.write(f"{now} | {twilight['name'].ljust(8)} | {twilight['status']}\n")
+    file.close
+# if log.txt does not exist, create it 
+else:
+    print("log.txt not found")
+    print("creating log.txt")
+    file = open("log.txt", "a")
+    file.close
